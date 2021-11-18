65cf52ea4d3d45529b46bd098774188b87d65226
Marc Hanheide <marc@hanheide.net>
2021-11-18 11:10:18 +0000

    Rebase from 'debian/melodic/sentor'

--- src/sentor/Executor.py
+++ src/sentor/Executor.py
@@ -7,6 +7,7 @@ Created on Thu Nov 21 10:30:22 2019
 #####################################################################################
 import rospy, rosservice, rostopic, actionlib, subprocess
 import dynamic_reconfigure.client
+import os, numpy, math
 from threading import Lock
 
 
@@ -53,21 +54,27 @@ class Executor(object):
             elif process_type == "lock_release":
                 self.init_lock_release(process)
                 
+            elif process_type == "custom":
+                self.init_custom(process)
+                
             else:
                 self.event_cb("Process of type '{}' not supported".format(process_type), "warn")
+                self.processes.append("not_initialised")
         
         self.default_indices = range(len(self.processes))        
-        print "\n"
                     
                     
     def init_call(self, process):
         
         try:
             service_name = process["call"]["service_name"]
+            service_name = self.get_name(service_name)
+                    
             service_class = rosservice.get_service_class_by_name(service_name)
-
-            rospy.wait_for_service(service_name, timeout=5.0)
-            service_client = rospy.ServiceProxy(service_name, service_class)
+            
+            timeout_srv = 1.0
+            if "timeout" in process["call"]:
+                timeout_srv = process["call"]["timeout"]
             
             req = service_class._request_class()
             for arg in process["call"]["service_args"]: exec(arg)
@@ -79,25 +86,27 @@ class Executor(object):
             d["func"] = "self.call(**kwargs)"
             d["kwargs"] = {}
             d["kwargs"]["service_name"] = service_name
-            d["kwargs"]["service_client"] = service_client
+            d["kwargs"]["service_class"] = service_class
             d["kwargs"]["req"] = req
             d["kwargs"]["verbose"] = self.is_verbose(process["call"])
+            d["kwargs"]["timeout_srv"] = timeout_srv
             
             self.processes.append(d)
             
         except Exception as e:
             self.event_cb(self.init_err_str.format("call", str(e)), "warn")
+            self.processes.append("not_initialised")
             
             
     def init_publish(self, process):
         
         try:
             topic_name = process["publish"]["topic_name"]
+            topic_name = self.get_name(topic_name)
             
-            if "topic_latched" in process["publish"].keys():
+            topic_latched = False
+            if "topic_latched" in process["publish"]:
                 topic_latched = process["publish"]["topic_latched"]
-            else:
-                topic_latched = False
             
             msg_class, real_topic, _ = rostopic.get_topic_class(topic_name)
             pub = rospy.Publisher(real_topic, msg_class, latch=topic_latched, 
@@ -119,6 +128,7 @@ class Executor(object):
             
         except Exception as e:
             self.event_cb(self.init_err_str.format("publish", str(e)), "warn")
+            self.processes.append("not_initialised")
             
             
     def init_action(self, process):
@@ -131,13 +141,14 @@ class Executor(object):
             exec("from {}.msg import {} as action_spec".format(package, spec))
             exec("from {}.msg import {} as goal_class".format(package, spec[:-6] + "Goal"))
             
-            rospy.sleep(0.1)
+            rospy.sleep(1.0)
             
             action_client = actionlib.SimpleActionClient(namespace, action_spec)
             wait = action_client.wait_for_server(rospy.Duration(5.0))
             if not wait:
-                e = "Action server with namespace '{}' and action spec '{}' not available".format(namespace, spec)
+                e = "Action server with namespace '{}' and action specification '{}' not available".format(namespace, spec)
                 self.event_cb(self.init_err_str.format("action", e), "warn")
+                self.processes.append("not_initialised")
                 return
     
             goal = goal_class()
@@ -146,18 +157,24 @@ class Executor(object):
             d = {}
             d["name"] = "action"
             d["verbose"] = self.is_verbose(process["action"])
-            d["def_msg"] = ("Sending goal for action with spec '{}'".format(spec), "info", goal)
+            d["def_msg"] = ("Sending goal for '{}' action with specification '{}'".format(namespace, spec), "info", goal)
             d["func"] = "self.action(**kwargs)"
             d["kwargs"] = {}
+            d["kwargs"]["namespace"] = namespace
             d["kwargs"]["spec"] = spec
             d["kwargs"]["action_client"] = action_client
             d["kwargs"]["goal"] = goal
             d["kwargs"]["verbose"] = self.is_verbose(process["action"])
+
+            d["kwargs"]["wait"] = False            
+            if "wait" in process["action"]:
+                d["kwargs"]["wait"] = process["action"]["wait"]
             
             self.processes.append(d)
         
         except Exception as e:
             self.event_cb(self.init_err_str.format("action", str(e)), "warn")
+            self.processes.append("not_initialised")
             
         
     def init_sleep(self, process):
@@ -174,7 +191,8 @@ class Executor(object):
             self.processes.append(d)
 
         except Exception as e:
-            self.event_cb(self.init_err_str.format("sleep", str(e)), "warn")            
+            self.event_cb(self.init_err_str.format("sleep", str(e)), "warn")
+            self.processes.append("not_initialised")            
             
             
     def init_shell(self, process):
@@ -187,11 +205,13 @@ class Executor(object):
             d["func"] = "self.shell(**kwargs)"
             d["kwargs"] = {}
             d["kwargs"]["cmd_args"] = process["shell"]["cmd_args"]
+            d["kwargs"]["shell_features"] = process["shell"]["shell_features"] if "shell_features" in process["shell"] else False
             
             self.processes.append(d)
 
         except Exception as e:
             self.event_cb(self.init_err_str.format("shell", str(e)), "warn")
+            self.processes.append("not_initialised")
 
 
     def init_log(self, process):
@@ -205,7 +225,7 @@ class Executor(object):
             d["kwargs"]["message"] = process["log"]["message"]
             d["kwargs"]["level"] = process["log"]["level"]
             
-            if "msg_args" in process["log"].keys():
+            if "msg_args" in process["log"]:
                 d["kwargs"]["msg_args"] = process["log"]["msg_args"]
             else:
                 d["kwargs"]["msg_args"] = None                
@@ -214,23 +234,36 @@ class Executor(object):
 
         except Exception as e:
             self.event_cb(self.init_err_str.format("log", str(e)), "warn")
+            self.processes.append("not_initialised")
             
             
     def init_reconf(self, process):
         
         try:
+            params = process["reconf"]["params"]
+            namespaces = set([param["namespace"] for param in params])
+            
+            default_config = {}
+            for namespace in namespaces:
+                rcnfclient = dynamic_reconfigure.client.Client(namespace, timeout=2.0)
+                default_config[namespace] = rcnfclient.get_configuration()
+                
+            default_params = [default_config[param["namespace"]][param["name"]] for param in params]
+            
             d = {}
             d["name"] = "reconf"
             d["verbose"] = self.is_verbose(process["reconf"])
-            d["def_msg"] = ("Reconfiguring parameters {}".format(process["reconf"]["params"]), "info", "")
+            d["def_msg"] = ("Reconfiguring parameters: {}".format(params), "info", "")
             d["func"] = "self.reconf(**kwargs)"
             d["kwargs"] = {}
-            d["kwargs"]["params"] = process["reconf"]["params"]
+            d["kwargs"]["params"] = params
+            d["kwargs"]["default_params"] = default_params
             
             self.processes.append(d)
 
         except Exception as e:
             self.event_cb(self.init_err_str.format("reconf", str(e)), "warn")
+            self.processes.append("not_initialised")
 
 
     def init_lock_acquire(self, process):
@@ -246,6 +279,7 @@ class Executor(object):
 
         except Exception as e:
             self.event_cb(self.init_err_str.format("lock_acquire", str(e)), "warn")
+            self.processes.append("not_initialised")
             
 
     def init_lock_release(self, process):
@@ -261,14 +295,63 @@ class Executor(object):
 
         except Exception as e:
             self.event_cb(self.init_err_str.format("lock_release", str(e)), "warn")
+            self.processes.append("not_initialised")
             
             
+    def init_custom(self, process):
+        
+        try:
+            package = process["custom"]["package"]
+            name = process["custom"]["name"]
+            
+            _file = name
+            if "file" in process["custom"]:
+                _file = process["custom"]["file"]
+            
+            exec("from {}.{} import {} as custom_proc".format(package, _file, name))
+            
+            if "init_args" in process["custom"]:
+                args = process["custom"]["init_args"] 
+                cp = custom_proc(*args)
+            else:
+                cp = custom_proc()
+            
+            d = {}
+            d["name"] = "custom"
+            d["verbose"] = self.is_verbose(process["custom"])
+            d["def_msg"] = ("Executing custom process '{}' from package '{}'".format(name, package), "info", "")
+            d["func"] = "self.custom(**kwargs)"
+            d["kwargs"] = {}
+            d["kwargs"]["cp"] = cp
+            
+            d["kwargs"]["args"] = None
+            if "run_args" in process["custom"]:
+                d["kwargs"]["args"] = process["custom"]["run_args"]
+            
+            self.processes.append(d)
+            
+        except Exception as e:
+            self.event_cb(self.init_err_str.format("custom", str(e)), "warn")
+            self.processes.append("not_initialised")
+
+        
+    def get_name(self, name):
+        
+        env_name = os.environ.get(name)
+        if env_name is not None:
+            name = env_name
+            
+        if rospy.has_param(name):
+            name = rospy.get_param(name)
+            
+        return name
+
+        
     def is_verbose(self, process):
         
-        if "verbose" in process.keys():
+        verbose = False
+        if "verbose" in process:
             verbose = process["verbose"]
-        else:
-            verbose = True
             
         return verbose
             
@@ -284,9 +367,13 @@ class Executor(object):
         
         for index in indices:
             rospy.sleep(0.1) # needed when using slackeros
+            
             process = self.processes[index]
+            if process == "not_initialised":
+                continue
+            
             try:
-                if process["verbose"] and "def_msg" in process.keys():
+                if process["verbose"] and "def_msg" in process:
                     self.event_cb(process["def_msg"][0], process["def_msg"][1], process["def_msg"][2])
                     
                 kwargs = process["kwargs"]            
@@ -296,8 +383,10 @@ class Executor(object):
                 self.event_cb("Unable to execute process of type '{}': {}".format(process["name"], str(e)), "warn")
             
 
-    def call(self, service_name, service_client, req, verbose):
+    def call(self, service_name, service_class, req, verbose, timeout_srv):
         
+        rospy.wait_for_service(service_name, timeout=timeout_srv)
+        service_client = rospy.ServiceProxy(service_name, service_class)
         resp = service_client(req)
         
         if verbose and resp.success:
@@ -310,22 +399,27 @@ class Executor(object):
         pub.publish(msg)
         
         
-    def action(self, spec, action_client, goal, verbose):
+    def action(self, namespace, spec, action_client, goal, verbose, wait):
         
+        self.action_namespace = namespace
         self.spec = spec
         self.goal = goal
         self.verbose_action = verbose
         
         action_client.send_goal(goal, self.goal_cb)
         
-       
+        if wait:
+            action_client.wait_for_result()
+            
+        
     def sleep(self, duration):
         rospy.sleep(duration)
         
         
-    def shell(self, cmd_args):
+    def shell(self, cmd_args, shell_features):
         
         process = subprocess.Popen(cmd_args,
+                     shell=shell_features,
                      stdout=subprocess.PIPE, 
                      stderr=subprocess.PIPE)
                      
@@ -341,17 +435,22 @@ class Executor(object):
         msg = self.msg
         if msg is not None and msg_args is not None:
             args = [eval(arg) for arg in msg_args]
-            args = tuple(args)
             self.event_cb("CUSTOM MSG: " + message.format(*args), level)
         else:
             self.event_cb("CUSTOM MSG: " + message, level)
             
             
-    def reconf(self, params):
+    def reconf(self, params, default_params):
         
-        for param in params:
-            rcnfclient = dynamic_reconfigure.client.Client(param["ns"], timeout=2.0)
-            rcnfclient.update_configuration({param["name"]: param["value"]})
+        for param, default_param in zip(params, default_params):            
+            rcnfclient = dynamic_reconfigure.client.Client(param["namespace"], timeout=1.0)
+            
+            if param["value"] != "_default":
+                value = param["value"]
+            else:
+                value = default_param
+                
+            rcnfclient.update_configuration({param["name"]: value})
             
             
     def lock_acquire(self):
@@ -360,14 +459,18 @@ class Executor(object):
 
     def lock_release(self):
         self._lock.release()
+        
+        
+    def custom(self, cp, args):
+        cp.run(*args) if args is not None else cp.run()
          
         
     def goal_cb(self, status, result):
         
         if self.verbose_action and status == 3:
-            self.event_cb("Goal achieved for action with spec '{}'".format(self.spec), "info", self.goal)
-        elif status == 2 or status == 6:
-            self.event_cb("Goal preempted for action with spec '{}'".format(self.spec), "warn", self.goal)
+            self.event_cb("Goal succeeded for '{}' action with specification '{}'".format(self.action_namespace, self.spec), "info", self.goal)
+        elif status == 2:
+            self.event_cb("Goal preempted for '{}' action with specification '{}'".format(self.action_namespace, self.spec), "warn", self.goal)
         elif status != 3:
-            self.event_cb("Goal failed for action with spec '{}'".format(self.spec), "warn", self.goal)
+            self.event_cb("Goal failed for '{}' action with specification '{}'. Status is {}".format(self.action_namespace, self.spec, status), "warn", self.goal)
 #####################################################################################
--- src/sentor/ROSTopicPub.py
+++ src/sentor/ROSTopicPub.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """
 @author: Francesco Del Duchetto (FDelDuchetto@lincoln.ac.uk)
+@author: Adam Binch (abinch@sagarobotics.com)
 
 """
 #####################################################################################
@@ -9,15 +10,25 @@ import rospy
 
 class ROSTopicPub(object):
 
-    def __init__(self, topic_name):
+    def __init__(self, topic_name, throttle_val):
 
         self.topic_name = topic_name
         self.pub_callbacks = []
+        self.throttle_val = throttle_val
+        self.throttle = self.throttle_val
 
     def callback_pub(self, msg):
 
         for func in self.pub_callbacks:
             func("'published'")
+            
+    def callback_pub_throttled(self, msg):
+        
+        if (self.throttle % self.throttle_val) == 0:
+            self.callback_pub(msg)
+            self.throttle = 1
+        else:
+            self.throttle += 1
 
     def register_published_cb(self, func):
 
